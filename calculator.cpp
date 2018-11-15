15456dc408a6750b0bc29750f05c2b2158999508
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:43:25 +0200

    add a multiplication operation

--- calculator.cpp
+++ calculator.cpp
@@ -1,5 +1,12 @@
 #include "calculator.h"
-
+//Bets D.O.
+//IO-05
+//Lab work 2
+//
+//(It starts with one) One thing, I don't know why
+//It doesn't even matter how hard you try
+//Keep that in mind, I designed this rhyme
+//To explain in due time all I know
 int Calculator::Add (double a, double b)
 {
 	return a + b + 0.5;
@@ -8,4 +15,9 @@ int Calculator::Add (double a, double b)
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
+}
+
+int Calculator::Mul (double a, double b)
+{
+    return a * b + 0.5;
 }
